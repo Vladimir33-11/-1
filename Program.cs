0eb13039c7ee37e0305243818a4ad2a66f88a2b5
Vladimir <Vladimir33-11@yandex.ru>
2022-11-08 11:55:30 +0400

    Ввели слова в массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿ string[]massiv;
+ int count;
+ string m;
+ string[]massiv2;
+ Console.WriteLine("Введите слова из группы");
+ count = 0;
+ massiv = new string[count];
+ do
+ {
+    m = Console.ReadLine();
+    if (m!="")
+    {
+        count++;
+        massiv2=new string[count];
+        for (int i=0; i<massiv2.Length-1; i++)
+        massiv2[i]=massiv[i];
+        massiv2[count-1]=m;
+        massiv=massiv2;
+    }
+ } while (m!="");
+ 
